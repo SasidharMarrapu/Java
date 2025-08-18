f0e9ee98ffb1923862cf3117bdd0dc85bf36ab83
SasidharMarrapu <sasidharmarrapu674@gmail.com>
2025-08-18 16:07:24 +0530

    arrays

---
+++ Sasidhar/src/arrays/reverse_array.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class reverse_array {
+	public static void main(String[] args) {
+		int a[] = {5,2,6,4,9,6,1};
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.println(a[i]);
+		}
+	}
+}

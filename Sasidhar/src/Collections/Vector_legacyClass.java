4ee5da534bf96a78986a0674f3689deb02cf9f1c
SasidharMarrapu <sasidharmarrapu674@gmail.com>
2025-08-29 11:58:58 +0530

    LegacyClass vector

---
+++ Sasidhar/src/Collections/Vector_legacyClass.java
@@ -0,0 +1,20 @@
+package Collections;
+import java.util.*;
+public class Vector_legacyClass {
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		v.add(10);
+		v.add(5);
+		v.add(20);
+		v.addElement(15);
+		Enumeration e = v.elements();
+		while(e.hasMoreElements()) {
+			System.out.println(e.nextElement());
+		}
+		Iterator i = v.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+	}
+}

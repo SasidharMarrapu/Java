b410aca056f6209407371446ca205393bdb527d7
SasidharMarrapu <sasidharmarrapu674@gmail.com>
2025-09-06 12:17:33 +0530

    update

---
+++ Sasidhar/src/tasks2/rectangle.java
@@ -0,0 +1,15 @@
+package tasks2;
+
+public class rectangle {
+	public static void main(String[] args) {
+		int rows = 4;
+		int cols = 6;
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<cols; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
